4fb2522bd9215d0a5ba26d5e071bd21155e163bf
Joren Janssens <jorenjanssens@me.com>
2023-08-16 15:28:05 +0200

    increased robustness & updated mail settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,41 +1,41 @@
-import { PlaywrightTestConfig } from '@playwright/test'
+import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
-  timeout: 120000,
-  retries: 0,
-  testDir: 'tests/',
-  reporter: [['list'], ['html', { open: 'never' }]],
-  globalTimeout: 60 * 60 * 1000,
-  expect: {
-    timeout: 60000
-  },
-  use: {
-    headless: true,
-    viewport: {
-      width: 1900,
-      height: 1050,
-    },
-    actionTimeout: 15000,
-    ignoreHTTPSErrors: true,
-    video: {
-      mode: 'retain-on-failure',
-      size: {
-        width: 1900,
-        height: 1050,
-      },
-    },
-    screenshot: 'only-on-failure',
-  },
-  projects: [
-    {
-      name: 'Chrome',
-      use: {
-        browserName: 'chromium',
-        launchOptions: {
-          args: ['--use-gs=egl'],
-        },
-      },
-    },
-  ],
-}
+	timeout: 120000,
+	retries: 2,
+	testDir: "tests/",
+	reporter: [["list"], ["html", { open: "never" }]],
+	globalTimeout: 60 * 60 * 1000,
+	expect: {
+		timeout: 60000,
+	},
+	use: {
+		headless: true,
+		viewport: {
+			width: 1900,
+			height: 1050,
+		},
+		actionTimeout: 15000,
+		ignoreHTTPSErrors: true,
+		video: {
+			mode: "retain-on-failure",
+			size: {
+				width: 1900,
+				height: 1050,
+			},
+		},
+		screenshot: "only-on-failure",
+	},
+	projects: [
+		{
+			name: "Chrome",
+			use: {
+				browserName: "chromium",
+				launchOptions: {
+					args: ["--use-gs=egl"],
+				},
+			},
+		},
+	],
+};
 
-export default config
+export default config;
